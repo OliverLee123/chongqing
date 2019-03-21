b7d9913b9536cb62cbcb546de800958fe2aadc3b
Yicong Li <33647891+OliverLee123@users.noreply.github.com>
2019-03-21 14:33:04 +0800

    Update __init__.py

--- extract/__init__.py
+++ extract/__init__.py
@@ -5,6 +5,6 @@ This package contains three algothrims to extract area.
 
 '''
 
-__all__ = ['expansion', 'frame', 'raster']
+__all__ = ['expansion', 'frame', 'raster', 'cluster']
 __author__ = 'Li Yicong'
 __affiliation__ = 'WHU' 
